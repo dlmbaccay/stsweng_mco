ac948c80fbf31c057f1539ed5ba830decdb1f31e
Andre <andre_aquino@dlsu.edu.ph>
2024-02-11 16:23:08 +0800

    chore: Existing files moved to parent folder

---
+++ src/components/theme-provider.jsx
@@ -0,0 +1,7 @@
+// components/ThemeProvider.js
+"use client";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
