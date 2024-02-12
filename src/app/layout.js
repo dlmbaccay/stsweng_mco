bee4cd5c5ba0a6f90f3f77384d00e876e5b25cf0
Dominic Baccay <91103278+dlmbaccay@users.noreply.github.com>
2024-02-12 20:33:08 +0800

    fix: fixed page routing and firebase config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast"
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}><ThemeProvider attribute="class" defaultTheme="system">{children}</ThemeProvider></body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          {children}
+          <Toaster 
+            position="top-right"
+            reverseOrder={false}
+          />
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
