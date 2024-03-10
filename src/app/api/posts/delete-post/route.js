f179718d4cf44393b3f454c8728906e32464192c
Andre <andre_aquino@dlsu.edu.ph>
2024-03-11 00:54:25 +0800

    chore: Cleaning the code for delete post

--- src/app/api/posts/delete-post/route.js
+++ src/app/api/posts/delete-post/route.js
@@ -7,11 +7,8 @@ export async function DELETE(request) {
     const body = await request.json();
     const {postID} = body;
 
-    console.log(postID)
-    
     try {
-        const message = await firestore.collection('posts').doc(postID).delete()
-        // const message = await deleteDocument('posts', postID );
+        const message = await deleteDocument('posts', postID );
         return NextResponse.json({message: message}, {status: 200});
     } catch (error) {
         console.log('Error in API Route:', error);
