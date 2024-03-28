90efc19104b96546a9176ba5baf493734c25306b
Andre <andre_aquino@dlsu.edu.ph>
2024-03-29 00:22:09 +0800

    fix: Fixed data fetching in timeline

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,243 +1,299 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 import Image from "next/image";
-import { auth, firestore} from "@/lib/firebase";
+import { auth, firestore } from "@/lib/firebase";
+import { getAllDocuments } from "@/lib/firestore-crud";
 import WithAuth from "@/components/WithAuth";
 import { ModeToggle } from "@/components/mode-toggle";
 import Loader from "@/components/Loader";
 import NavBar from "@/components/nav/navbar";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import { CreatePost } from "@/components/post-components/create-post";
 import { PostSnippet } from "@/components/post-components/post-snippet";
 import { RepostSnippet } from "@/components/post-components/repost-snippet";
 
 import {
-    collection,
-    query,
-    orderBy,
-    limit,
-    onSnapshot,
-    startAfter,
-    getDocs,
-    where,
-  } from "firebase/firestore";
+	collection,
+	query,
+	orderBy,
+	limit,
+	onSnapshot,
+	startAfter,
+	getDocs,
+	where,
+} from "firebase/firestore";
 
 function HomePage() {
-    const [userData, setUserData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [ currentUser, setCurrentUser ] = useState([]);
+	const [userData, setUserData] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [currentUser, setCurrentUser] = useState([]);
+	const [userPets, setUserPets] = useState([]);
 
-    const [ activeTab, setActiveTab ] = useState('posts');
-    const [ userPosts, setUserPosts ] = useState([]);
-    const urlParams = useParams();
+	const [activeTab, setActiveTab] = useState("For You");
+	const [userPosts, setUserPosts] = useState([]);
+	const urlParams = useParams();
 
+	useEffect(() => {
+		setLoading(true);
+		/**
+		 * Subscribes to authentication state changes.
+		 * @type {function}
+		 */
+		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				// User is signed in
+				// Fetch signed-in user's data
+				try {
+					/**
+					 * Fetches the data for the currently signed-in user.
+					 * @param {string} userId - The ID of the signed-in user.
+					 * @returns {Promise<object>} The user data.
+					 */
+					const fetchCurrentUser = async (userId) => {
+						const response = await fetch(
+							`/api/users/via-id?id=${userId}`,
+							{
+								method: "GET",
+							}
+						);
+						if (response.ok) {
+							const data = await response.json();
+							setCurrentUser(data);
+						} else {
+							// Assuming the API returns { message: '...' } on error
+							const errorData = await response.json();
+							throw new Error(errorData.message);
+						}
+					};
+
+					await fetchCurrentUser(user.uid);
+				} catch (error) {
+					console.error("Error fetching current user data:", error);
+				} finally {
+					setLoading(false);
+				}
+			} else {
+				// User is signed out
+				setLoading(false);
+			}
+		});
+
+		return unsubscribe; // Clean-up function for the observer
+	}, []);
+
+	/**
+     * This useEffect hook is responsible for fetching and updating the data of the current user.
+     * It subscribes to changes in the current user's data in the Firestore database and updates the current user's data on the page accordingly.
+     * The cleanup function is returned to unsubscribe from the Firestore listener when the component unmounts.
+     * This is for the viewer of the page.
+     */
     useEffect(() => {
-        setLoading(true); 
-        /**
-         * Subscribes to authentication state changes.
-         * @type {function}
-         */
-        const unsubscribe = auth.onAuthStateChanged(async (user) => {
-            if (user) { // User is signed in
-                // Fetch signed-in user's data
-                try {
-                    /**
-                     * Fetches the data for the currently signed-in user.
-                     * @param {string} userId - The ID of the signed-in user.
-                     * @returns {Promise<object>} The user data.
-                     */
-                    const fetchCurrentUser = async (userId) => {
-                        const response = await fetch(`/api/users/via-id?id=${userId}`, {
-                            method: 'GET'
-                        });
-                        if (response.ok) {
-                            const data = await response.json();
-                            setCurrentUser(data);
-                        } else {
-                            // Assuming the API returns { message: '...' } on error
-                            const errorData = await response.json();
-                            throw new Error(errorData.message);
-                        }
-                    };
-
-                    await fetchCurrentUser(user.uid);
-                } catch (error) {
-                    console.error('Error fetching current user data:', error);
-                } finally { 
-                    setLoading(false);
+        let unsubscribe;
+    
+        if (currentUser) { // info of the current user
+            const userRef = firestore.collection('users').doc(currentUser.uid);
+            // Whenever the user's data in the database changes, update the user's data on the page
+            unsubscribe = userRef.onSnapshot((doc) => {
+                const newData = doc.data();
+                // Prevent infinite loop by setting data only when there is a difference
+                if (JSON.stringify(newData) !== JSON.stringify(currentUser)) {
+                    setCurrentUser(newData);
                 }
-            } else { // User is signed out
-                setLoading(false);
-            }
-        });
-    
-        return unsubscribe; // Clean-up function for the observer
-    }, []);
-
-    useEffect(() => {
-        // get current user using auth and firestore
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-          if (user) {
-            firestore.collection('users').doc(user.uid).get().then((doc) => {
-              if (doc.exists) {
-                setCurrentUser(doc.data());
-              } else {
-                console.log('No such document!');
-              }
-            }).catch((error) => {
-              console.log('Error getting document:', error);
             });
-          } else {
-            setCurrentUser(null);
-          }
-        });
+        } 
     
-        return () => {
-          unsubscribe();
-        }
-      }, []);
+        return () => unsubscribe; // Cleanup function
+    }, [currentUser]);
 
-    const [ fetchedFollowing, setFetchedFollowing ] = useState([]);
-    const [ fetchedPosts, setFetchedPosts ] = useState([]);
+	/**
+	 * This useEffect hook is responsible for fetching and updating the pets of the user whose profile is being viewed.
+	 * It fetches the pets of the user from the Firestore database and updates the user's pets on the page accordingly.
+	 *
+	 */
+	useEffect(() => {
+		// Fetch user pets
+		if (currentUser) {
+			const fetchUserPets = async () => {
+				const response = await fetch(
+					`/api/pets/retrieve-user-pets?uid=${currentUser.uid}`,
+					{
+						method: "GET", // Specify GET method
+					}
+				);
 
-    const [allPosts, setAllPosts] = useState([]);
-    const [allPostsLoaded, setAllPostsLoaded] = useState(false);
-    const [allPostsLastVisible, setAllPostsLastVisible] = useState(null);
+				if (response.ok) {
+					const data = await response.json();
+					setUserPets(data.userPets);
+				} else {
+					// Assuming the API returns { message: '...' } on error
+					const errorData = await response.json();
+					throw new Error(errorData.message);
+				}
+			};
 
-    const [following, setFollowing] = useState([]); 
-    const [followingPosts, setFollowingPosts] = useState([]);
-    const [followingPostsLoaded, setFollowingPostsLoaded] = useState(false);
-    const [followingLastVisible, setFollowingLastVisible] = useState(null);
+			fetchUserPets();
+		}
+	}, [currentUser]);
 
-    useEffect(() => {
+	const [allPosts, setAllPosts] = useState([]);
+	const [allPostsLoaded, setAllPostsLoaded] = useState(false);
+	const [allPostsLastVisible, setAllPostsLastVisible] = useState(null);
+
+	const [followingPosts, setFollowingPosts] = useState([]);
+	const [followingPostsLoaded, setFollowingPostsLoaded] = useState(false);
+	const [followingLastVisible, setFollowingLastVisible] = useState(null);
+
+	useEffect(() => {
         setLoading(true);
-        const fetchPosts = async () => {
-            const allPosts = [];
-            const following = [];
-      
-            // fetch all posts
-            const allPostsQuery = query(
-              collection(firestore, 'posts')
-            );
-            const allPostsSnapshot = await getDocs(allPostsQuery);
-            allPostsSnapshot.forEach((doc) => {
-              allPosts.push(doc.data());
-            });
-      
-            // fetch following posts
-            const followingQuery = query(
-              collection(firestore, 'posts')
-            //   , 
-            //   where('petIDs', 'array-contains', urlParams.petID),
-            //   where('category', '==', 'Milestones')
-            );
-            const followingSnapshot = await getDocs(followingQuery);
-            followingSnapshot.forEach((doc) => {
-              following.push(doc.data());
-            });
-      
-            setFetchedPosts(allPosts);
-            setFetchedFollowing(following);
-          }
-      
-        //   fetchData();
-          fetchPosts();
+        if (currentUser) {
+            const fetchData = async () => {
+                // fetch all posts
+                const results = await getAllDocuments('posts');
+                setAllPosts(results);
+                const filteredPosts = await results.filter(post => currentUser.following && currentUser.following.includes(post.authorID) || (post.petIDs && post.petIDs.some(petID => currentUser.following && currentUser.following.includes(petID))));
+                setFollowingPosts(filteredPosts);
+                console.log(followingPosts)
+            };
+            fetchData();
+            setLoading(false);
+        }
+	}, [currentUser]);
 
-    }, []);
+	return (
+		<>
+			{loading ? (
+				<Loader show={true} />
+			) : (
+				currentUser && (
+					<div className="flex">
+						{/* Side Navbar */}
+						<div className="min-h-16 w-full z-50 fixed">
+							<NavBar
+								props={{
+									uid: currentUser.uid,
+									username: currentUser.username,
+									userPhotoURL: currentUser.userPhotoURL,
+									expand_lock: true,
+								}}
+							/>
+						</div>
 
-  return (
-    <>
-      { loading ? <Loader show={true} /> : (currentUser && 
-          <div className="flex justify-center h-screen">
-              {/* Side Navbar */}
-              <div className="min-h-16 w-full z-50 fixed">
-                  <NavBar props={{
-                      uid : currentUser.uid,
-                      username: currentUser.username, 
-                      userPhotoURL: currentUser.userPhotoURL,
-                      expand_lock: true,
-                  }}/>
-              </div>
+						<div className="w-full h-full z-10 mt-16 flex items-center flex-col">
+							{/* Tabs */}
+							<div className="mt-6 mb-6 flex flex-row font-bold w-full md:w-1/2 h-[35px] text-sm bg-off_white dark:bg-gray drop-shadow-md rounded-l-sm rounded-r-sm gap-1">
+								<div
+									className={`transition-all w-1/2 flex items-center justify-center rounded-l-sm ${
+										activeTab == "For You"
+											? "bg-muted_blue dark:bg-light_yellow text-white dark:text-black"
+											: "hover:bg-inherit hover:border-2 hover:border-primary hover:text-primary cursor-pointer transition-all"
+									}`}
+									onClick={() => setActiveTab("For You")}
+								>
+									For You
+								</div>
 
-              <div className="w-1/2 h-full z-10 mt-16 flex justify-center flex-col">
-                
-                {/* Tabs */}
-                <div className="mt-6 mb-6 flex flex-row font-bold w-1/2 h-[35px] text-sm bg-off_white dark:bg-gray drop-shadow-md rounded-l-sm rounded-r-sm gap-1">
-                    <div
-                        className={`transition-all w-1/2 flex items-center justify-center rounded-l-sm ${activeTab == 'posts' ? "bg-muted_blue dark:bg-light_yellow text-white dark:text-black" : "hover:bg-inherit hover:border-2 hover:border-primary hover:text-primary cursor-pointer transition-all"}`} 
-                        onClick={() => setActiveTab('For You')}
-                    >
-                        For You
-                    </div>
+								<div
+									className={`transition-all w-1/2 flex items-center justify-center rounded-r-sm ${
+										activeTab == "Following"
+											? "bg-muted_blue dark:bg-light_yellow text-white dark:text-black"
+											: "hover:bg-inherit hover:border-2 hover:border-primary hover:text-primary cursor-pointer transition-all"
+									}`}
+									onClick={() => setActiveTab("Following")}
+								>
+									Following
+								</div>
+							</div>
 
-                    <div
-                        className={`transition-all w-1/2 flex items-center justify-center rounded-r-sm ${activeTab == 'pets' ? "bg-muted_blue dark:bg-light_yellow text-white dark:text-black" : "hover:bg-inherit hover:border-2 hover:border-primary hover:text-primary cursor-pointer transition-all"}`} 
-                        onClick={() => setActiveTab('Following')}
-                    >
-                        Following
-                    </div>
-                </div>
+							{/* Create Post */}
+							<div className="w-full md:w-1/2">
+								{currentUser && (
+									<Card className="drop-shadow-md rounded-sm mb-6">
+										<div className="flex flex-row items-center w-full my-2">
+											<div className="ml-4">
+												<Image
+													src={currentUser.userPhotoURL == "" ? "/images/profilePictureHolder.jpg" : currentUser.userPhotoURL}
+													alt="user photo"
+													width={44}
+													height={44}
+													className="rounded-full aspect-square object-cover"
+												/>
+											</div>
+											<div className="w-full mr-4">
+												<CreatePost
+													props={{
+														uid: currentUser.uid,
+														username: currentUser.username,
+														displayname: currentUser.displayName,
+														userphoto: currentUser.userPhotoURL,
+														pets: userPets,
+													}}
+												/>
+											</div>
+										</div>
+									</Card>
+								)}
 
-                {/* Create Post */}
-                <div className="">
-                    { currentUser.uid === userData.uid ? 
-                        <Card className="drop-shadow-md rounded-sm mb-6">
-                            <div className="flex flex-row items-center w-full my-2">
-                                <div className="ml-4">
-                                    <Image src={userData.userPhotoURL == "" ? "/images/profilePictureHolder.jpg" : userData.userPhotoURL} alt="user photo" width={44} height={44} className="rounded-full aspect-square object-cover" />
-                                </div>
-                                <div className="w-full mr-4">
-                                    <CreatePost props={{
-                                        uid: userData.uid,
-                                        username: userData.username,
-                                        displayname: userData.displayName,
-                                        userphoto: userData.userPhotoURL,
-                                        pets: userPets,
-                                    }}/>
-                                </div>
-                            </div>
-                        </Card> : null
-                    }
-
-                    {activeTab == 'For You' ? (
-                        <>
-                            <div className="flex flex-col min-w-full items-center justify-center gap-6">
-                                {fetchedPosts.map((post) => {
-                                    return (
-                                        (post.postType == 'Original' ?
-                                            <PostSnippet key={post.postID} post={post} currentUser={currentUser} />
-                                        
-                                        : post.postType == 'Repost' ?
-                                            <RepostSnippet key={post.postID} post={post} currentUser={currentUser} />
-                                        : null)
-                                    )
-                                })}
-                            </div>
-                        </>
-                    ): (
-                        <>
-                            <div className="flex flex-col min-w-full items-center justify-center gap-6">
-                                {fetchedPosts.map((post) => {
-                                    return (
-                                        (post.postType == 'Original' ?
-                                            <PostSnippet key={post.postID} post={post} currentUser={currentUser} />
-                                        
-                                        : post.postType == 'Repost' ?
-                                            <RepostSnippet key={post.postID} post={post} currentUser={currentUser} />
-                                        : null)
-                                    )
-                                })}
-                            </div>
-                        </>
-                    )}
-                </div>
-              </div>
-          </div>
-      )}
-    </>
-  )
+								{activeTab == "For You" ? (
+									<>
+										<div className="flex flex-col min-w-full items-center justify-center gap-6">
+											{allPosts.map((post) => {
+												return post.postType ==
+													"Original" ? (
+													<PostSnippet
+														key={post.postID}
+														post={post}
+														currentUser={currentUser}
+													/>
+												) : post.postType ==
+												  "Repost" ? (
+													<RepostSnippet
+														key={post.postID}
+														post={post}
+														currentUser={currentUser}
+													/>
+												) : null;
+											})}
+										</div>
+									</>
+								) : (
+									<>
+										<div className="flex flex-col min-w-full items-center justify-center gap-6">
+											{followingPosts.map((post) => {
+												return post.postType ==
+													"Original" ? (
+													<PostSnippet
+														key={post.postID}
+														post={post}
+														currentUser={currentUser}
+													/>
+												) : post.postType ==
+												  "Repost" ? (
+													<RepostSnippet
+														key={post.postID}
+														post={post}
+														currentUser={currentUser}
+													/>
+												) : null;
+											})}
+										</div>
+									</>
+								)}
+							</div>
+						</div>
+					</div>
+				)
+			)}
+		</>
+	);
 }
 
 export default WithAuth(HomePage);
